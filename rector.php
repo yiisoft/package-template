9d92391fe49f35d22f4d5b40ba7b1440f9fcafa1
Alexey Rogachev <arogachev90@gmail.com>
2023-05-30 18:02:17 +0600

    Exclude undefined variable rule in Rector (#89)

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\LevelSetList;
 
@@ -23,5 +24,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->skip([
         ClosureToArrowFunctionRector::class,
+        AddDefaultValueForUndefinedVariableRector::class,
     ]);
 };
