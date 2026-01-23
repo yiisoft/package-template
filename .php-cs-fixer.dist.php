fb80392e960ca03095ca986a350eba3bb6ebeadf
Максим Спирков <spirkov.2001@mail.ru>
2026-01-23 23:36:53 +0400

    CR Fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,39 +2,20 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Yiisoft\CodeStyle\ConfigBuilder;
 
 $finder = (new Finder())->in([
     __DIR__ . '/src',
     __DIR__ . '/tests',
 ]);
 
-return (new Config())
+return ConfigBuilder::build()
     ->setRiskyAllowed(true)
     ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
-        '@PER-CS3.0' => true,
-        'no_unused_imports' => true,
-        'no_extra_blank_lines' => [
-            'tokens' => [
-                'curly_brace_block',
-                'extra',
-            ],
-        ],
-        'ordered_class_elements' => true,
-        'class_attributes_separation' => ['elements' => ['method' => 'one']],
-        'declare_strict_types' => true,
-        'native_function_invocation' => true,
-        'native_constant_invocation' => true,
-        'fully_qualified_strict_types' => [
-            'import_symbols' => true
-        ],
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
+        '@Yiisoft/Core' => true,
+        '@Yiisoft/Core:risky' => true,
     ])
     ->setFinder($finder);
